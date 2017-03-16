c6428e2f9ab1e0fc278b34c5a05b0bce42f1f1ce
James Chant <jamesrchant@gmail.com>
2017-03-16 22:06:29 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,9 @@
 class ProfilesController < ApplicationController
+  # Before any of these actions get run, do this function (from Devise)
+  before_action :authenticate_user!
+  # We define in private 
+  before_action :only_current_user
+  
   
   # GET to /users/:user_id/profile/new
   def new
@@ -45,5 +50,10 @@ class ProfilesController < ApplicationController
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  # Before any of these actions get run, do this function (from Devise)
+  before_action :authenticate_user!
   
   # GET to /users/:user_id
   def show
     @user = User.find( params[:id] )
   end
+
 end
