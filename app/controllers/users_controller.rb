39d4f32088e2ee61d97d918f6abecd4077893260
James Chant <jamesrchant@gmail.com>
2017-03-16 22:34:21 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
   # Before any of these actions get run, do this function (from Devise)
   before_action :authenticate_user!
+
+  def index
+    # We could do Users.all but that runs individual SQL queries which is expensive
+    # This optimises the database retrieval
+    @users = User.includes(:profile)
+  end
   
   # GET to /users/:user_id
   def show
     @user = User.find( params[:id] )
   end
 
-  def index
-    
-  end
-  
 end
